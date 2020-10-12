ac7037fbcef8361ea8da48db4788b3d5b42829c3
Crew Spence <csspence123@gmail.com>
2020-10-12 05:43:40 -0500

    Working solution.

--- codewars_sort_and_star.js
+++ codewars_sort_and_star.js
@@ -7,16 +7,5 @@ You should not remove or add elements from/to the array.
 */
 
 const twoSort = (s) => {
-  let sorted = s.sort(function(a, b) {
-    return a.charCodeAt(0) - b.charCodeAt(0);
-  });
-  let finalString = '';
-  for(let i = 0; i < sorted[0].length; i++) {
-    finalString += sorted[0][i];
-    if(i < sorted[0].length - 1) {
-      finalString += '***';
-    }
-  }
-
-  return finalString;
+  return s.sort()[0].split('').join('***');
 }
