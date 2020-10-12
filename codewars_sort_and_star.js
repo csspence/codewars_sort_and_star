38442532931534a06056fb2af247f3aa346b1995
Crew Spence <csspence123@gmail.com>
2020-10-12 05:41:02 -0500

    Sorts according to ACII code, but still not fully functional.

--- codewars_sort_and_star.js
+++ codewars_sort_and_star.js
@@ -6,6 +6,17 @@ The returned value must be a string, and have "***" between each of its letters.
 You should not remove or add elements from/to the array.
 */
 
-function twoSort(s) {
+const twoSort = (s) => {
+  let sorted = s.sort(function(a, b) {
+    return a.charCodeAt(0) - b.charCodeAt(0);
+  });
+  let finalString = '';
+  for(let i = 0; i < sorted[0].length; i++) {
+    finalString += sorted[0][i];
+    if(i < sorted[0].length - 1) {
+      finalString += '***';
+    }
+  }
 
+  return finalString;
 }
